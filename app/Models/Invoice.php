f7d25abd9c4deb0c5e4bfb2a8baff865f46bc64a
acer <rahmanimahdi16@gmail.com>
2024-11-28 23:04:17 +0330

    update models and migrations

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property mixed $amount
+ * @property mixed $amount
+ * @property mixed $user_id
+ */
 class Invoice extends Model
 {
+    public $fillable = [
+        'amount',
+        'user_id',
+    ];
 
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class,'invoice_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
--- database/migrations/2024_11_28_130953_create_invoices_table.php
+++ database/migrations/2024_11_28_130953_create_invoices_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('subscription_plan_id');
             $table->integer('amount');
+            $table->integer('balance');
+            $table->foreignId('user_id')->index();
             $table->timestamps();
         });
     }
