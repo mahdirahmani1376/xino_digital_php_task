6ee9e878bd1056c4a7ef5631acf6ec35309525d6
acer <rahmanimahdi16@gmail.com>
2024-11-28 20:55:15 +0330

    add custom Exception lang

---
+++ lang/en/exceptions.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    "user_does_have_required_subscription_plan"=> "this user must have this minimum subscription name :subscription_name",
+];
---
+++ lang/fa/exceptions.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    "user_does_have_required_subscription_plan"=> "شما باید دارای حداقل سطح کاربری  :subscription_name باشید",
+];
