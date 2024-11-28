133f7bb7336dfed1bc5d25d3d80f11ffb167f2c3
acer <rahmanimahdi16@gmail.com>
2024-11-28 23:07:06 +0330

    register payment routes

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,7 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed $invoice_id
+ * @property mixed $amount
+ * @property mixed $status
+ * @property mixed $trace_id
+ */
 class Transaction extends Model
 {
-    //
+    public $fillable = [
+        'invoice_id',
+        'amount',
+        'status',
+        'trace_id'
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/payment/{transaction}/success', [PaymentController::class, 'success'])->name('payment.success');
+Route::get('/payment/{transaction}/cancel', [PaymentController::class, 'cancel'])->name('payment.cancel');
+
