b5f784f20464642b8fe1f52580653558f9106b05
acer <rahmanimahdi16@gmail.com>
2024-11-28 20:55:28 +0330

    modify models

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+/**
+ * @property $name
+ * @property $subscripition_plan_id
+ */
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Section extends Model
 {
-    //
+    public function subscriptionPlan(): BelongsTo  
+    {
+        return $this->belongsTo(SubscriptionPlan::class,'subscription_plan_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property $subscription_id
+ * @property $name
+ * @property $email
+ * @property $password
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -21,6 +28,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'subscription_id'
     ];
 
     /**
@@ -44,5 +52,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function subscription(): BelongsTo
+    {
+        return $this->belongsTo(Subscription::class);
     }
 }
