8e5c58a50f3dc2f7bdb853bade050ea60d633a40
acer <rahmanimahdi16@gmail.com>
2024-11-29 16:36:37 +0330

    mock http requests

--- app/Providers/MockHttpRequestsServiceProvider.php
+++ app/Providers/MockHttpRequestsServiceProvider.php
@@ -28,9 +28,18 @@ class MockHttpRequestsServiceProvider extends ServiceProvider
     private function mockHttpRequests(): void
     {
         Http::fake([
-            'https://api.sandbox.paypal.com/v1/payments/payment/execute' => Http::response([
-                'id' => 'PAYID-MOCK123456789',
+            config('payment.paypal.base_url') .'/execute' => Http::response([
+                'paymentId' => 'PAYID-MOCK123456789',
                 'state' => 'approved',
+            ], 200),
+            config('payment.paypal.base_url') .'/v1/billing/plans' => Http::response([
+                'state' => 'success',
+            ], 200),
+            config('payment.paypal.base_url') .'/v1/payments/payment' => Http::response([
+                'paymentId' => 'PAYID-MOCK123456789',
+            ], 200),
+            config('payment.paypal.base_url') .'/v1/oauth2/token' => Http::response([
+                'access_token' => 'test',
             ], 200),
         ]);
     }
