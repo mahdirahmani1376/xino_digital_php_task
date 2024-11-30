d875839b121552ddb7978c70a7378c61572acde3
acer <rahmanimahdi16@gmail.com>
2024-12-01 00:21:23 +0330

    refactor validaiton logic

--- app/Http/Middleware/SectionMiddleware.php
+++ app/Http/Middleware/SectionMiddleware.php
@@ -16,37 +16,38 @@ class SectionMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next,$section): Response
+    public function handle(Request $request, Closure $next,$section)
     {
-        $this->validate($section);
 
-        return $next($request);
-    }
-
-    private function validate(string $section)
-    {
         $user = Auth::user();
         $section = Section::firstWhere('name',$section);
         $subscription = $user->subscription;
 
+        // dd($user->subscription_id,$subscription->toArray(),$section->toArray());
+
         if (empty($subscription))
         {
-            throw new SubscriptionPlanException(
-                trans('exceptions.user_does_have_any_subscription_plan')
-            );
+            return response()->json([
+                'message' => trans('exceptions.user_does_have_any_subscription_plan')
+            ], 404);
+
         }
         
         if ($section->subscriptionPlan?->priority > $user->subscription?->subscriptionPlan?->priority ){
-            throw new SubscriptionPlanException(
-                trans('exceptions.user_does_have_required_subscription_plan',[
-                'subscription_name' => $section->subscriptionPlan?->name
-                ]));
+            return response()->json([
+                'message' => trans('exceptions.user_does_have_required_subscription_plan',[
+                    'subscription_name' => $section->subscriptionPlan?->name
+                    ])
+            ], 403);
         }
 
         if ($user->subscription?->expired_at < now() ){
-            throw new SubscriptionPlanException(
-                trans('exceptions.users_subscription_is_expired')
-            );
+            return response()->json([
+                'message' => trans('exceptions.users_subscription_is_expired')
+            ], 403);
         }
+
+        return $next($request);
     }
+
 }
